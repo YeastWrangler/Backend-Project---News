bd51788a8d5db57434e0b75f169aeb71f2e90bdd
Alex <102862775+YeastWrangler@users.noreply.github.com>
2022-08-01 15:40:55 +0100

    Update topic.controllers.js

--- controllers/topic.controllers.js
+++ controllers/topic.controllers.js
@@ -5,7 +5,6 @@ const {fetchTopics} = require('../models/topic.models')
 exports.getTopics = (req, res, next) => {
     fetchTopics()
     .then((topics) => {
-        console.log(topics)
         res.status(200).send({topics});
     }).catch(next)
 };
