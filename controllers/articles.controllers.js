18d4c720f53d45c51f58c774dff1cf8519da3fe9
YeastWrangler <102862775+YeastWrangler@users.noreply.github.com>
2022-08-02 08:47:46 +0100

    adds GET by article function with testing task -4

---
+++ controllers/articles.controllers.js
@@ -0,0 +1,8 @@
+const {fetchArticles} = require('../models/articles.models')
+
+exports.getArticlesById = (req, res, next) => {
+    const {article_id} = req.params
+    fetchArticles(article_id).then((article) => {
+        res.status(200).send({article})
+    }).catch(next);
+};
---
+++ models/articles.models.js
@@ -0,0 +1,12 @@
+const db = require('../db/connection')
+
+exports.fetchArticles = (id) => {
+    return db
+    .query(`SELECT * FROM articles WHERE article_id = $1;`, [id])
+    .then(({rows})=> {
+        if(rows.length === 0) {
+          return Promise.reject({status: 404, msg: 'article ID does not exist'})
+        } else 
+        return rows;
+        })
+}; 
