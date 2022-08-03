cab634e18fd11d3bf6f56ce76430254f0e2a2cfb
YeastWrangler <102862775+YeastWrangler@users.noreply.github.com>
2022-08-03 09:28:07 +0100

    api/articles refactored to now return comment count -ticket-7

--- models/articles.models.js
+++ models/articles.models.js
@@ -2,7 +2,11 @@ const db = require('../db/connection')
 
 exports.fetchArticles = (id) => {
     return db
-    .query(`SELECT * FROM articles WHERE article_id = $1;`, [id])
+    .query(`SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.body, articles.created_at, articles.votes, COUNT(comments.comment_id) AS comment_count
+    FROM articles 
+    LEFT JOIN comments ON comments.article_id = articles.article_id
+    WHERE articles.article_id=$1
+    GROUP BY comments.article_id, articles.article_id;`,[id])
     .then(({rows: article})=> {
         if(article.length === 0) {
           return Promise.reject({status: 404, msg: 'article ID not found'})
